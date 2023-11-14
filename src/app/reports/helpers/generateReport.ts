106bccc9b0286f8cf009874ad4ac3baafcfcfe1e
Ali Kehel <ali.saber.27.2@gmail.com>
2023-11-14 19:04:23 +0200

    fix: fix a bug

--- src/app/reports/helpers/generateReport.ts
+++ src/app/reports/helpers/generateReport.ts
@@ -1,9 +1,9 @@
-import { jsPDF } from "jspdf";
+// import { jsPDF } from "jspdf";
 
-export const generateReport = async () => {
-    // Default export is a4 paper, portrait, using millimeters for units
-    const doc = new jsPDF();
+// export const generateReport = async () => {
+//     // Default export is a4 paper, portrait, using millimeters for units
+//     const doc = new jsPDF();
 
-    doc.text("Hello world!", 10, 10);
-    doc.save("temp.pdf");
-};
+//     doc.text("Hello world!", 10, 10);
+//     doc.save("temp.pdf");
+// };
