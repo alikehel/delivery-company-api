33a8960adb82b1d2883dc12680c403e40e1fa817
Ali Kehel <ali.saber.27.2@gmail.com>
2024-01-07 08:09:33 +0200

    feat: add default images in db

---
+++ src/database/migrations/20240107060903_/migration.sql
@@ -0,0 +1,14 @@
+-- AlterTable
+ALTER TABLE "Banner" ALTER COLUMN "image" SET DEFAULT 'https://w7.pngwing.com/pngs/819/548/png-transparent-photo-image-landscape-icon-images.png';
+
+-- AlterTable
+ALTER TABLE "Company" ALTER COLUMN "logo" SET DEFAULT 'https://w7.pngwing.com/pngs/307/22/png-transparent-computer-icons-business-company-corporation-company-angle-company-text.png';
+
+-- AlterTable
+ALTER TABLE "Product" ALTER COLUMN "image" SET DEFAULT 'https://w7.pngwing.com/pngs/436/721/png-transparent-package-delivery-box-freight-transport-computer-icons-shipping-miscellaneous-angle-freight-transport.png';
+
+-- AlterTable
+ALTER TABLE "Store" ALTER COLUMN "logo" SET DEFAULT 'https://w7.pngwing.com/pngs/307/22/png-transparent-computer-icons-business-company-corporation-company-angle-company-text.png';
+
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "avatar" SET DEFAULT 'https://w7.pngwing.com/pngs/627/693/png-transparent-computer-icons-user-user-icon.png';
