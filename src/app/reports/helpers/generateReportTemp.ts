1293a73c4b1830739a6db751d210890396ccc979
Ali Kehel <ali.saber.27.2@gmail.com>
2024-02-21 16:21:25 +0200

    fix(reports): fix a typo

--- src/app/reports/helpers/generateReportTemp.ts
+++ src/app/reports/helpers/generateReportTemp.ts
@@ -178,7 +178,7 @@ export const generateReport = async (
                                     // TODO
                                     text: reportData.clientReport?.store
                                         ? handleArabicCharacters(
-                                              `الصفحة: ${reportData.clientReport.store.name}}`
+                                              `الصفحة: ${reportData.clientReport.store.name}`
                                           )
                                         : ""
                                 },
