0339dfb8e89055f5b6d98ade11750c613db4e2e5
Ali Kehel <ali.saber.27.2@gmail.com>
2024-02-25 13:55:49 +0200

    refactor: change NODE_ENV

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -102,9 +102,9 @@ export default (
     err.statusCode = err.statusCode || 500;
     err.status = err.status || "error";
 
-    if (process.env.NODE_ENV === "dev") {
+    if (process.env.NODE_ENV === "development") {
         sendErrorDev(err, res);
-    } else if (process.env.NODE_ENV === "prod") {
+    } else if (process.env.NODE_ENV === "production") {
         let error = { ...err };
         // console.log(error);
 
