6f182b303c3af370f0f56d3445be719bd43cbe39
Ali Kehel <ali.saber.27.2@gmail.com>
2024-02-25 18:04:48 +0200

    feat(pdf): display english digits and currencies in arabic

--- src/lib/generateHTML.ts
+++ src/lib/generateHTML.ts
@@ -4,7 +4,7 @@ import { Logger } from "../lib/logger";
 
 export const generateHTML = async (template: string, data: object) => {
     try {
-        handlebars.registerHelper("date", (date) => new Date(date).toLocaleDateString());
+        handlebars.registerHelper("date", (date) => new Date(date).toLocaleDateString("ar-IQ"));
         handlebars.registerHelper("mapPhones", (phones) => {
             if (!phones) return "";
             if (typeof phones === "string") return phones;
@@ -12,6 +12,15 @@ export const generateHTML = async (template: string, data: object) => {
         });
         handlebars.registerHelper("inc", (value) => parseInt(value) + 1);
         handlebars.registerHelper("add", (v1, v2) => parseInt(v1) + parseInt(v2));
+        handlebars.registerHelper("currency", (value) => {
+            return Number(value || 0).toLocaleString("ar-IQ", { style: "currency", currency: "IQD" });
+        });
+        handlebars.registerHelper("arabicNumber", (value) => {
+            const arabicNumbers = ["٠", "١", "٢", "٣", "٤", "٥", "٦", "٧", "٨", "٩"];
+            if (!value) return "";
+            if (typeof value === "string") return value.replace(/[0-9]/g, (w) => arabicNumbers[+w]);
+            return value.toString().replace(/[0-9]/g, (w: string) => arabicNumbers[+w]);
+        });
 
         const compiledTemplate = handlebars.compile(template, { strict: true });
         const html = compiledTemplate({
