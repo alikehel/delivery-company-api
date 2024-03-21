5ab9b836a30f6c7f2a505eac63710ea6007ed8a1
Ali Kehel <ali.saber.27.2@gmail.com>
2024-03-21 08:55:21 +0200

    fix(companies): json stringify the form data in company create request

--- src/app/companies/companies.zod.ts
+++ src/app/companies/companies.zod.ts
@@ -2,31 +2,33 @@
 import { generateSchema } from "@anatine/zod-openapi";
 import { z } from "zod";
 
-export const CompanyCreateSchema = z.object({
-    companyData: z.object({
-        name: z.string().min(3),
-        phone: z.string().regex(/^07[3-9][0-9]{8}$/),
-        website: z.string().url().optional(),
-        logo: z.string().optional(),
-        color: z.string().toUpperCase().length(6).optional(),
-        registrationText: z.string().optional(),
-        governoratePrice: z.coerce.number().min(0),
-        deliveryAgentFee: z.coerce.number().min(0),
-        baghdadPrice: z.coerce.number().min(0),
-        additionalPriceForEvery500000IraqiDinar: z.coerce.number().min(0),
-        additionalPriceForEveryKilogram: z.coerce.number().min(0),
-        additionalPriceForRemoteAreas: z.coerce.number().min(0),
-        orderStatusAutomaticUpdate: z.coerce.boolean().optional()
-    }),
-    companyManager: z.object({
-        username: z.string().min(3),
-        name: z.string().min(3),
-        password: z.string().min(6),
-        phone: z.string().regex(/^07[3-9][0-9]{8}$/),
-        // salary: z.coerce.number().min(0),
-        avatar: z.string().optional()
+export const CompanyCreateSchema = z
+    .object({
+        companyData: z.object({
+            name: z.string().min(3),
+            phone: z.string().regex(/^07[3-9][0-9]{8}$/),
+            website: z.string().url().optional(),
+            logo: z.string().optional(),
+            color: z.string().toUpperCase().length(6).optional(),
+            registrationText: z.string().optional(),
+            governoratePrice: z.coerce.number().min(0),
+            deliveryAgentFee: z.coerce.number().min(0),
+            baghdadPrice: z.coerce.number().min(0),
+            additionalPriceForEvery500000IraqiDinar: z.coerce.number().min(0),
+            additionalPriceForEveryKilogram: z.coerce.number().min(0),
+            additionalPriceForRemoteAreas: z.coerce.number().min(0),
+            orderStatusAutomaticUpdate: z.coerce.boolean().optional()
+        }),
+        companyManager: z.object({
+            username: z.string().min(3),
+            name: z.string().min(3),
+            password: z.string().min(6),
+            phone: z.string().regex(/^07[3-9][0-9]{8}$/),
+            // salary: z.coerce.number().min(0),
+            avatar: z.string().optional()
+        })
     })
-});
+    .refine((data) => JSON.stringify(data));
 
 export type CompanyCreateType = z.infer<typeof CompanyCreateSchema>;
 
