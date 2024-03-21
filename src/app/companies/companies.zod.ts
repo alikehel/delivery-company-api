6050055481fe70aeff8eab92eaa7002c1c317d7c
Ali Kehel <ali.saber.27.2@gmail.com>
2024-03-21 11:31:09 +0200

    fix(companies): fix a bug

--- src/app/companies/companies.zod.ts
+++ src/app/companies/companies.zod.ts
@@ -2,46 +2,51 @@
 import { generateSchema } from "@anatine/zod-openapi";
 import { z } from "zod";
 
+/*******************************************************************************
+ * Empty string needs to be converted to null
+ *******************************************************************************/
 export const CompanyCreateSchema = z.preprocess(
     (data) => {
-        console.info(data);
-        return JSON.parse(data as string);
+        return {
+            // @ts-expect-error
+            companyData: JSON.parse(data.companyData),
+            // @ts-expect-error
+            companyManager: JSON.parse(data.companyManager)
+        };
     },
     z.object({
-        companyData: z.preprocess(
-            (data) => {
-                console.info(data);
-                // JSON.parse(data as string);
-            },
-            z.object({
-                name: z.string().min(3),
-                phone: z.string().regex(/^07[3-9][0-9]{8}$/),
-                website: z.string().url().optional(),
-                logo: z.string().optional(),
-                color: z.string().toUpperCase().length(6).optional(),
-                registrationText: z.string().optional(),
-                governoratePrice: z.coerce.number().min(0),
-                deliveryAgentFee: z.coerce.number().min(0),
-                baghdadPrice: z.coerce.number().min(0),
-                additionalPriceForEvery500000IraqiDinar: z.coerce.number().min(0),
-                additionalPriceForEveryKilogram: z.coerce.number().min(0),
-                additionalPriceForRemoteAreas: z.coerce.number().min(0),
-                orderStatusAutomaticUpdate: z.coerce.boolean().optional()
-            })
-        ),
-        companyManager: z.preprocess(
-            (data) => {
-                console.info(data);
-                // JSON.parse(data as string);
-            },
-            z.object({
-                username: z.string().min(3),
-                name: z.string().min(3),
-                password: z.string().min(6),
-                phone: z.string().regex(/^07[3-9][0-9]{8}$/),
-                avatar: z.string().optional()
-            })
-        )
+        companyData: z.object({
+            name: z.string().min(3),
+            phone: z.string().regex(/^07[3-9][0-9]{8}$/),
+            website: z.preprocess((data) => (data === "" ? undefined : data), z.string().url().optional()),
+            logo: z.preprocess((_data) => {
+                return "";
+            }, z.string().optional()),
+            color: z.preprocess(
+                (data) => (data === "" ? undefined : data),
+                z.string().toUpperCase().length(6).optional()
+            ),
+            registrationText: z.preprocess((data) => (data === "" ? undefined : data), z.string().optional()),
+            governoratePrice: z.coerce.number().min(0),
+            deliveryAgentFee: z.coerce.number().min(0),
+            baghdadPrice: z.coerce.number().min(0),
+            additionalPriceForEvery500000IraqiDinar: z.coerce.number().min(0),
+            additionalPriceForEveryKilogram: z.coerce.number().min(0),
+            additionalPriceForRemoteAreas: z.coerce.number().min(0),
+            orderStatusAutomaticUpdate: z.preprocess(
+                (data) => (data === "" ? undefined : data),
+                z.coerce.boolean().optional()
+            )
+        }),
+        companyManager: z.object({
+            username: z.string().min(3),
+            name: z.string().min(3),
+            password: z.string().min(6),
+            phone: z.string().regex(/^07[3-9][0-9]{8}$/),
+            avatar: z.preprocess((_data) => {
+                return "";
+            }, z.string().optional())
+        })
     })
 );
 
