1e90c21f3d8bcd5910427c13651e71c7be574ffd
Ali Kehel <ali.saber.27.2@gmail.com>
2024-04-03 08:54:54 +0200

    refactor(clients): improve clients code

--- src/app/clients/clients.repository.ts
+++ src/app/clients/clients.repository.ts
@@ -1,83 +1,8 @@
-import { Prisma } from "@prisma/client";
 import { prisma } from "../../database/db";
 import { ClientCreateTypeWithUserID, ClientUpdateType } from "./clients.dto";
+import { clientReform, clientSelect } from "./clients.responses";
 
-const clientSelect = {
-    user: {
-        select: {
-            id: true,
-            name: true,
-            username: true,
-            phone: true,
-            avatar: true
-        }
-    },
-    role: true,
-    governoratesDeliveryCosts: true,
-    createdBy: {
-        select: {
-            id: true,
-            name: true
-        }
-    },
-    repository: {
-        select: {
-            id: true,
-            name: true
-        }
-    },
-    branch: {
-        select: {
-            id: true,
-            name: true
-        }
-    },
-    company: {
-        select: {
-            id: true,
-            name: true,
-            logo: true,
-            color: true
-        }
-    },
-    deleted: true,
-    deletedAt: true,
-    deletedBy: {
-        select: {
-            id: true,
-            name: true
-        }
-    }
-} satisfies Prisma.ClientSelect;
-
-const clientReform = (
-    client: Prisma.ClientGetPayload<{
-        select: typeof clientSelect;
-    }> | null
-) => {
-    if (!client) {
-        return null;
-    }
-    return {
-        // TODO
-        id: client.user.id,
-        name: client.user.name,
-        username: client.user.username,
-        phone: client.user.phone,
-        avatar: client.user.avatar,
-        role: client.role,
-        company: client.company,
-        repository: client.repository,
-        branch: client.branch,
-        governoratesDeliveryCosts: client.governoratesDeliveryCosts,
-        createdBy: client.createdBy,
-        deleted: client.deleted,
-        deletedBy: client.deleted && client.deletedBy,
-        deletedAt: client.deletedAt?.toISOString()
-    };
-};
-
-export class ClientModel {
+export class ClientsRepository {
     async createClient(companyID: number, data: ClientCreateTypeWithUserID) {
         const createdUser = await prisma.user.create({
             data: {
