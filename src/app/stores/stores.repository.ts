521cf579576a626eb7c5a9e882658f7500e79395
Ali Kehel <ali.saber.27.2@gmail.com>
2024-04-07 17:55:49 +0200

    refactor(stores): improve stores code

--- src/app/stores/stores.repository.ts
+++ src/app/stores/stores.repository.ts
@@ -1,81 +1,8 @@
-import { Prisma } from "@prisma/client";
 import { prisma } from "../../database/db";
 import { StoreCreateType, StoreUpdateType } from "./stores.dto";
+import { storeSelect, storeSelectReform } from "./stores.responses";
 
-const storeSelect = {
-    id: true,
-    name: true,
-    notes: true,
-    logo: true,
-    client: {
-        select: {
-            user: {
-                select: {
-                    id: true,
-                    name: true
-                }
-            }
-        }
-    },
-    clientAssistant: {
-        select: {
-            user: {
-                select: {
-                    id: true,
-                    name: true
-                }
-            }
-        }
-    },
-    company: {
-        select: {
-            id: true,
-            name: true
-        }
-    },
-    deleted: true,
-    deletedAt: true,
-    deletedBy: {
-        select: {
-            id: true,
-            name: true
-        }
-    }
-} satisfies Prisma.StoreSelect;
-
-const storeSelectReform = (
-    store: Prisma.StoreGetPayload<{
-        select: typeof storeSelect;
-    }> | null
-) => {
-    if (!store) {
-        return null;
-    }
-    return {
-        id: store.id,
-        name: store.name,
-        notes: store.notes,
-        logo: store.logo,
-        client: store.client.user
-            ? {
-                  id: store.client.user.id,
-                  name: store.client.user.name
-              }
-            : undefined,
-        clientAssistant: store.clientAssistant
-            ? {
-                  id: store.clientAssistant.user.id,
-                  name: store.clientAssistant.user.name
-              }
-            : undefined,
-        company: store.company,
-        deleted: store.deleted,
-        deletedBy: store.deleted && store.deletedBy,
-        deletedAt: store.deletedAt?.toISOString()
-    };
-};
-
-export class StoreModel {
+export class StoresRepository {
     async createStore(companyID: number, data: StoreCreateType) {
         const createdStore = await prisma.store.create({
             data: {
