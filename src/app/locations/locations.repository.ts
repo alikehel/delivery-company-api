eb6b6a1d07335a70fba170fe6a176511352c36cd
Ali Kehel <ali.saber.27.2@gmail.com>
2024-05-29 19:59:56 +0300

    update locations.repository.ts

--- src/app/locations/locations.repository.ts
+++ src/app/locations/locations.repository.ts
@@ -27,7 +27,7 @@ export class LocationsRepository {
                               };
                           })
                       }
-                    : undefined,
+                    : undefined
                 // company: {
                 //     connect: {
                 //         id: companyID
